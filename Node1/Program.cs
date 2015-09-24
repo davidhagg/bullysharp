213d62eda7896495375459f5a60e3ab82032e3be
David Hägg <david.hagg@saabgroup.com>
2015-09-25 00:16:06 +0200

    Added Node1 and cleaned up

---
+++ Node1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using BullySharp.Core;
+using BullySharp.Core.Logging;
+
+namespace Node1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var logger = new ConsoleLogger();
+            var bully = new Bully(logger);
+            bully.LeaderChanged += Bully_LeaderChanged;
+
+            bully.Start();
+
+            Console.ReadLine();
+
+            bully.Stop();
+        }
+
+        private static void Bully_LeaderChanged(object sender, int e)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine($"NEW LEADER: {e}");
+            Console.ResetColor();
+        }
+    }
+}
